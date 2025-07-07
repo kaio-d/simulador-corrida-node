c0a13785df7a9288c46f7d76e68fbb06b22220c8
kaio-d <dantaskaio01@gmail.com>
2025-07-07 20:25:04 -0300

    create function randonBlock and raceEngine

--- src/index.js
+++ src/index.js
@@ -22,4 +22,55 @@ async function rollDice() {
   console.log(
     `🏁 🚨 Corrida entre ${player1.nome} e ${player2.nome} começando ... \n`
   );
+
+  await playRaceEngine(player1, player2);
 })();
+
+async function playRaceEngine(playerOne, playerTwo) {
+  for (let i = 1; i <= 5; i++) {
+    console.log(`🏁 Rodada ${i}`);
+
+    let block = await getRandomBlock();
+    console.log(`Bloco: ${block}`);
+  }
+
+  let diceResult1 = await rollDice();
+  let diceResult2 = await rollDice();
+
+  let totalTestSkill1 = 0;
+  let totalTestSkill2 = 0;
+
+  if (result === "Reta") {
+    totalTestSkill1 = diceResult1 + player1.velocidade;
+    totalTestSkill2 = diceResult2 + player2.velocidade;
+  }
+
+  if (result === "Curva") {
+    totalTestSkill1 = diceResult1 + player1.manobrabilidade;
+    totalTestSkill2 = diceResult2 + player2.manobrabilidade;
+  }
+
+  if (result === "Confronto") {
+  }
+}
+
+async function getRandomBlock() {
+  let random = Math.random();
+  let result = "";
+
+  switch (true) {
+    case random < 0.33:
+      result = "Reta";
+      break;
+
+    case random < 0.66:
+      result = "Curva";
+      break;
+
+    default:
+      result = "Confronto";
+      break;
+  }
+
+  return result;
+}
